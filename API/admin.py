86f6b0267276b605ccaf2cb7fa57f3bd5674fe53
Julio Hernández <me@jchernandez88.com>
2013-10-26 14:16:17 -0500

    Agrega list_display en operacion

--- API/admin.py
+++ API/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from models import Operacion
 
-admin.site.register(Operacion)
+class OperacionAdmin(admin.ModelAdmin):
+    list_display = ('op1','operacion','op2','resultado')
+        
+admin.site.register(Operacion,OperacionAdmin)
