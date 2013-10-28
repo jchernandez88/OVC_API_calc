78583e5bb2c6b792fb19cc3b1584977e4e1c5f17
Julio Hernández <me@jchernandez88.com>
2013-10-27 23:04:20 -0600

    Agrega el calculo de las operaciones básicas "+-*/"

--- API/models.py
+++ API/models.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 from django.db import models
 
@@ -9,10 +8,46 @@ class Operacion(models.Model):
         (u'*', u'Multiplicación'),
         (u'/', u'División'),
     )
-    op1 = models.DecimalField(max_digits=14,decimal_places=4)
-    op2 = models.DecimalField(max_digits=14,decimal_places=4)
-    operacion = models.CharField(max_length=1, choices=OPERACIONES)
-    resultado = models.DecimalField(max_digits=14,decimal_places=4)
+    op1 = models.DecimalField(
+        max_digits=14,
+        decimal_places=4,
+        default=0.0,
+        help_text='Operador 1',
+        )
+    op2 = models.DecimalField(
+        max_digits=14,
+        decimal_places=4,
+        default=0.0,
+        help_text='Operador 2',
+        )
+    operacion = models.CharField(
+        max_length=1,
+        choices=OPERACIONES,
+        default='+',
+        help_text='Seleccione una operación',
+        )
+    resultado = models.DecimalField(
+        max_digits=14,
+        decimal_places=4,
+        null=True,
+        blank=True,
+        help_text='El resultado de la operación',
+        )
 
     def __unicode__(self):
         return "%s %s %s = %s" % (self.op1,self.operacion,self.op2,self.resultado)
+
+    def save(self):
+        res = 0.0
+        if self.operacion == '*':
+            res = self.op1 * self.op2
+        elif self.operacion == '/':
+            if self.op2 != 0:
+                res = self.op1 / self.op2
+        elif self.operacion == '-':
+            res = self.op1 - self.op2
+        else:
+            res = self.op1 + self.op2
+        self.resultado = res
+        super (Operacion, self).save()
+
